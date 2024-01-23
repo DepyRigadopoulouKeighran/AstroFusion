c05b1679a1f0ee2af3a9c22e671b449abdff3974
kiriakosVangel <kiriakos_vaggeltzikis@hotmail.com>
2024-01-23 16:00:23 +0100

    Trying to print the data of the planets

---
+++ src/components/Mercury/Mercury.jsx
@@ -0,0 +1,27 @@
+const Mercury = () => {
+  return (
+    <>
+      <header className="cardHeader">
+        <h1>Mercury</h1>
+        <nav className="navLink">
+          <ul className="navUl">
+            <li className="navLi">OVERVIEW</li>
+            <li className="navLi">IN-DEPTH</li>
+            <li className="navLi">NUMBERS</li>
+            <li className="navLi">IMAGES</li>
+          </ul>
+        </nav>
+        <img src="" alt="" />
+        <h3>LENGTH OF YEAR</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam hic
+          temporibus exercitationem suscipit, harum doloremque incidunt iure quo
+          fuga, debitis quas sapiente porro provident soluta perspiciatis
+          laboriosam animi dolore nobis.
+        </p>
+      </header>
+    </>
+  );
+};
+
+export default Mercury;
---
+++ src/data/Data.js
@@ -0,0 +1,71 @@
+const planetsData = [
+  {
+    name: 'Mercury',
+    id: 'one',
+    info: 'Mercury is the smallest and innermost planet in the Solar System...',
+    distance: '57.9 million km',
+    funFacts:
+      'Mercury has a day longer than its year, rotating three times for every two orbits around the Sun.',
+  },
+  {
+    name: 'Venus',
+    id: 'two',
+    info: "Venus is the second planet from the Sun and is often called Earth's 'sister planet'...",
+    distance: '108.2 million km',
+    funFacts:
+      'Venus has a thick atmosphere, making it the hottest planet in our solar system.',
+  },
+  {
+    name: 'Earth',
+    id: 'three',
+    info: 'Earth is the third planet from the Sun and the only astronomical object known to harbor life...',
+    distance: '149.6 million km',
+    funFacts: 'Earth is the only known celestial body where life exists.',
+  },
+  {
+    name: 'Mars',
+    id: 'four',
+    info: "Mars is the fourth planet from the Sun, often referred to as the 'Red Planet'...",
+    distance: '227.9 million km',
+    funFacts:
+      'Mars has the tallest volcano and the deepest, longest canyon in the solar system.',
+  },
+  {
+    name: 'Jupiter',
+    id: 'five',
+    info: 'Jupiter is the largest planet in our solar system and the fifth from the Sun...',
+    distance: '778 million km',
+    funFacts: 'Jupiter has a strong magnetic field and more than 70 moons.',
+  },
+  {
+    name: 'Saturn',
+    id: 'six',
+    info: 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System...',
+    distance: '1.4 billion km',
+    funFacts: "Saturn's iconic rings are made up of ice particles and dust.",
+  },
+  {
+    name: 'Uranus',
+    id: 'seven',
+    info: 'Uranus is the seventh planet from the Sun and the third-largest in the Solar System...',
+    distance: '2.9 billion km',
+    funFacts: 'Uranus is unique among the planets as it rotates on its side.',
+  },
+  {
+    name: 'Neptune',
+    id: 'eight',
+    info: 'Neptune is the eighth and farthest-known Solar planet from the Sun...',
+    distance: '4.5 billion km',
+    funFacts:
+      'Neptune has the strongest winds in the solar system, reaching up to 1,300 mph.',
+  },
+  {
+    name: 'Pluto',
+    id: 'nine',
+    info: 'Pluto is a dwarf planet in our Solar System, and it used to be considered the ninth planet...',
+    distance: '5.9 billion km',
+    funFacts:
+      'Pluto was reclassified as a dwarf planet in 2006 by the International Astronomical Union.',
+  },
+];
+export default planetsData;
