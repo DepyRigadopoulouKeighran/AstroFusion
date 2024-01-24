9a23d38c4f1609a07ec9a3f9beb15dfe1e0a4d97
Depy Rigadopoulou-Keighran <lightblue@duck.com>
2024-01-24 09:54:29 +0100

    added earth.jsx, css and images

--- src/components/Earth/Earth.jsx
+++ src/components/Earth/Earth.jsx
@@ -15,3 +15,97 @@ const Earth = () => {
 };
 
 export default Earth;
+
+// This is the front page
+
+// import "../../src/App.css";
+// import satelite from "../images/earth-satelite.gif";
+
+// const EarthFront = () => {
+//   return (
+//     <>
+//       <h1 className="cardHeaderFront">Earth</h1>
+
+//       <nav className="cardNav">
+//         <ul className="navUl">
+//           <li className="navLi">
+//             <a href="#size">Size</a>
+//           </li>
+//           <li className="navLi">
+//             <a href="#distance">Distance from the Sun</a>
+//           </li>
+//           <li className="navLi">
+//             <a href="#characteristics">Key Characteristics</a>
+//           </li>
+//         </ul>
+//       </nav>
+//       <img className="cardImageFront" src={satelite} alt="" />
+//     </>
+//   );
+// };
+
+// export default EarthFront;
+
+// This is the back page
+
+// import earth from "../images/earth1.gif";
+// import "../../src/App.css";
+
+// function EarthBack() {
+//   return (
+//     <>
+//       <h1 className="cardHeaderBack">Earth</h1>
+
+//       <nav className="cardNav">
+//         <ul className="navUl">
+//           <li className="navLi">
+//             <a href="#size">Size</a>
+//           </li>
+//           <li className="navLi">
+//             <a href="#distance">Distance from the Sun</a>
+//           </li>
+//           <li className="navLi">
+//             <a href="#characteristics">Key Characteristics</a>
+//           </li>
+//         </ul>
+//       </nav>
+//       <img className="cardImageBack" src={earth} alt="" />
+//       <section className="cardSection">
+//         <h2 className="cardTitles">Fun facts about Earth</h2>
+//         <p className="cardText">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta illum
+//           maiores adipisci saepe nam accusantium rerum, hic vel expedita
+//           excepturi ut optio architecto laborum, minima aliquam inventore est
+//           voluptatum. Vel. Lorem ipsum dolor sit amet, consectetur adipisicing
+//           elit. Dicta illum maiores adipisci saepe nam accusantium rerum, hic
+//           vel expedita excepturi ut optio architecto laborum, minima aliquam
+//           inventore est voluptatum. Vel.
+//         </p>
+
+//         <h2 className="cardTitles">Historical Events</h2>
+//         <p className="cardText">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta illum
+//           maiores adipisci saepe nam accusantium rerum, hic vel expedita
+//           excepturi ut optio architecto laborum, minima aliquam inventore est
+//           voluptatum. Vel. Lorem ipsum dolor sit amet, consectetur adipisicing
+//           elit. Dicta illum maiores adipisci saepe nam accusantium rerum, hic
+//           vel expedita excepturi ut optio architecto laborum, minima aliquam
+//           inventore est voluptatum. Vel.
+//         </p>
+
+//         <h2 className="cardTitles">Space Phenomena</h2>
+//         <p className="cardText">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta illum
+//           maiores adipisci saepe nam accusantium rerum, hic vel expedita
+//           excepturi ut optio architecto laborum, minima aliquam inventore est
+//           voluptatum. Vel. Lorem ipsum dolor sit amet, consectetur adipisicing
+//           elit. Dicta illum maiores adipisci saepe nam accusantium rerum, hic
+//           vel expedita excepturi ut optio architecto laborum, minima aliquam
+//           inventore est voluptatum. Vel.
+//         </p>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default EarthBack;
